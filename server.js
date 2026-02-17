c19962f5fe1a25c9e7283ae8b6126dff0f96ad9f
Sagar878796 <sagarsutradhar046@gmail.com>
2026-02-18 02:58:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,23 +3,45 @@ const https = require("https");
 
 const PORT = process.env.PORT || 10000;
 
-http.createServer((req, res) => {
-  if (req.url === "/playlist.php" || req.url === "/playlist") {
-    
-    const playlist = `#EXTM3U
-#EXTINF:-1 tvg-name="Test Channel",Test Channel
-https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8
-`;
+const PLAYLIST_URL = "https://la.drmlive.net/tp/playlist.php";
 
-    res.writeHead(200, {
-      "Content-Type": "application/x-mpegURL"
+http.createServer((req, res) => {
+
+  if (req.url === "/playlist.php" || req.url === "/playlist") {
+
+    https.get(PLAYLIST_URL, (response) => {
+
+      let data = "";
+
+      response.on("data", chunk => {
+        data += chunk;
+      });
+
+      response.on("end", () => {
+
+        res.writeHead(200, {
+          "Content-Type": "application/x-mpegURL",
+          "Access-Control-Allow-Origin": "*"
+        });
+
+        res.end(data);
+
+      });
+
+    }).on("error", (err) => {
+
+      res.writeHead(500);
+      res.end("Error loading playlist");
+
     });
-    res.end(playlist);
 
   } else {
+
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Tataplay proxy running");
+    res.end("Tataplay Render Proxy Running");
+
   }
+
 }).listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
